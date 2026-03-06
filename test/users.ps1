6a6c70b867f08102ec416edc107c9b65cfbb9e7d
vernonthedev <techjaja2@gmail.com>
2026-03-06 13:07:03 +0300

    test: added get user requests

--- test/users.ps1
+++ test/users.ps1
@@ -1,9 +1,19 @@
-# Users API request Testing
 
-# Creating a new user
-$uri = "http://localhost:3000/api/v1/users"
+<#
+--------------------------------------
+Users API request Testing
+--------------------------------------
+#>
+
+# CREATE USER
+$id = 4
+$uri = "http://localhost:3000/api/v1/users/$id"
 $body = @{
     name="samuel"
 } | ConvertTo-Json
 
-Invoke-RestMethod -Uri $uri -Method Post -ContentType "application/json" -Body $body
+# Invoke-RestMethod -Uri $uri -Method Post -ContentType "application/json" -Body $body
+
+# GET USERS
+Invoke-RestMethod -Uri $uri -Method Get
+
